76368c978b053f1915ae0d0878df668485758447
Sergey Korobka <nejdancs@gmail.com>
2022-06-01 00:16:02 +0300

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const ref = {
+  form: document.querySelector('.form'),
+};
+
+ref.form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const {
+    target: { delay, step, amount },
+  } = e;
+
+  let delayVal = Number(delay.value);
+  const stepVal = Number(step.value);
+  const amountVal = Number(amount.value);
+
+  for (let i = 1; i <= amountVal; i += 1, delayVal += stepVal) {
+    createPromise(i, delayVal)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
